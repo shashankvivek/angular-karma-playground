98fc1a77ddb656860be26b4f9932b75569af33aa
Shashank Vivek <SVivek2@slb.com>
2019-04-03 19:42:38 +0530

    added spec file for service

--- karma-project/src/app/app.component.spec.ts
+++ karma-project/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NoticeBoardComponent } from './notice-board/notice-board.component';
+import { StudentsComponent } from './students/students.component';
+import { HttpClientModule } from '@angular/common/http';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [HttpClientModule],
+      declarations: [AppComponent, NoticeBoardComponent, StudentsComponent],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -17,11 +19,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('karma-project');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to karma-project!');
   }));
 });
