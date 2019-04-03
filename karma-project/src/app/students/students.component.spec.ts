35cb54a1645dc4dc7ac24f59c3bf34d6b17f770c
Shashank Vivek <SVivek2@slb.com>
2019-04-03 11:48:14 +0530

    added test cases

--- karma-project/src/app/students/students.component.spec.ts
+++ karma-project/src/app/students/students.component.spec.ts
@@ -4,6 +4,7 @@ import { StudentsComponent } from './students.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StudentsService } from './students.service';
 import { StudentsServiceStub } from './students.service.mock';
+import { By } from '@angular/platform-browser';
 
 describe('StudentsComponent', () => {
   let component: StudentsComponent;
@@ -23,7 +24,32 @@ describe('StudentsComponent', () => {
     fixture.detectChanges();
   });
 
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
   it('should have "user_list" populated ', () => {
-    expect(component.user_list.length).toBe(3);
+    expect(component.user_list.length).toBeGreaterThan(0);
+  });
+
+  it('should call getUserList() of StudentService on component Init', () => {
+    spyOn(component._userService, 'getUserList').and.callThrough();
+    component.ngOnInit();
+    expect(component._userService.getUserList).toHaveBeenCalled();
+  });
+
+  it('should render User List in HTML', () => {
+    const ele = fixture.debugElement.query(By.css('.users-div')).nativeElement;
+    // console.log(ele.childNodes[1].innerHTML)
+    expect(ele.childNodes[1].innerHTML).not.toBeNull();
+    expect(ele.childNodes[2].innerHTML).not.toBeNull();
+    expect(ele.childNodes[3].innerHTML).not.toBeNull();
+  });
+
+  describe('should have User Board', () => {
+    it('empty by default', () => {
+      const ele = fixture.debugElement.nativeElement.querySelector('#board');
+      expect(ele.innerHTML.trim()).toBe('paid : 0');
+    });
   });
 });
